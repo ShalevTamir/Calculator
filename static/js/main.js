f7f17bed728fa50cd76705c424175f5b84fdaaf6
Shalev Tamir <shalevtamir1009@gmail.com>
2023-10-09 09:35:49 +0300

    Working Version

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,31 @@
 const currentDisplay = document.querySelector('#display #current');
 const operationDisplay = document.querySelector('#display #operation')
+const displayParent = document.querySelector('#display');
 const DEFAULT_CHARACTER = '0';
 const EQUALS_CHARACTER = '=';
+const DECIMAL_CHARACTER = '.';
+const DECREASE_FONT_AMOUNT = '10';
 var doingOperation = false;
-var resetNumber = false;
-var operationComplete = false;
+var resetCurrentDisplay = false;
+var operationCompleted = false;
+var operations = ['/','*','-','+'];
+var currentDisplayFontSize = parseFloat(window.getComputedStyle(currentDisplay).getPropertyValue('font-size'));
+var operationDisplayFontSize =  parseFloat(window.getComputedStyle(operationDisplay).getPropertyValue('font-size'));
+
 
 function init(){
     currentDisplay.textContent = DEFAULT_CHARACTER;
+    
     document.querySelectorAll('.number').forEach((button)=>{
         button.addEventListener('click',()=>{
-            if(resetNumber || operationComplete){
+            if(resetCurrentDisplay || operationCompleted){
                 currentDisplay.textContent = button.textContent;
-                if(resetNumber){
-                    resetNumber = false;
+                if(resetCurrentDisplay){
+                    resetCurrentDisplay = false;
                 }
-                if(operationComplete){
+                if(operationCompleted){
                     operationDisplay.textContent = '';
-                    operationComplete = false;
+                    operationCompleted = false;
                 }
             }
             else{
@@ -27,10 +35,14 @@ function init(){
     })    
     document.querySelectorAll('.operator').forEach((button)=>{
         button.addEventListener('click',()=>{
+            if(operationCompleted){
+                operationDisplay.textContent = currentDisplay.textContent;
+                operationCompleted = false;
+            }
             if(!doingOperation){
                 operationDisplay.textContent=currentDisplay.textContent+button.textContent;
                 doingOperation = true;
-                resetNumber = true;
+                resetCurrentDisplay = true;
             }
             else{
                 operationDisplay.textContent = replaceCharacter(operationDisplay.textContent,
@@ -46,24 +58,74 @@ function init(){
             currentDisplay.textContent = DEFAULT_CHARACTER;
     })
     document.querySelector('#btn-result').addEventListener('click',()=>{
-        if(operationDisplay.textContent.includes(EQUALS_CHARACTER)){
-            replaceFirstOperationNumber(currentDisplay.textContent);
-        }
+        if(currentDisplay.textContent.charAt(currentDisplay.textContent.length-1) == DECIMAL_CHARACTER)
+            currentDisplay.textContent = currentDisplay.textContent.slice(0,-1);
         result = computeResult();
-        operationDisplay.textContent += currentDisplay.textContent + EQUALS_CHARACTER;
+        if(result == "Infinity"){
+            alert('Invalid operation or number out of range');
+            return;
+        }
+        if(!operationCompleted)
+        {
+            operationDisplay.textContent += currentDisplay.textContent + EQUALS_CHARACTER;
+            operationCompleted = true;
+        }
         currentDisplay.textContent = result;
         doingOperation = false;
-        operationComplete = true;
+        resetCurrentDisplay = true;
     })
+    document.querySelector('#btn-clear-partial').addEventListener('click',()=>{
+        if(operationCompleted){
+            resetAll();
+        }
+        else{
+            currentDisplay.textContent = DEFAULT_CHARACTER;
+        }
+    })
+    document.querySelector('#btn-clear-all').addEventListener('click',resetAll)
+    document.querySelector('#btn-change-sign').addEventListener('click',()=>{
+        currentDisplay.textContent = -1 * parseInt(currentDisplay.textContent); 
+    })
+    document.querySelector('#btn-decimal').addEventListener('click',()=>{
+        if(!currentDisplay.textContent.includes(DECIMAL_CHARACTER))
+            currentDisplay.textContent += DECIMAL_CHARACTER;
+    })
+    document.querySelectorAll('.btn').forEach((button)=>{
+        button.addEventListener('click',()=>{
+            [[currentDisplay, currentDisplayFontSize], [operationDisplay, operationDisplayFontSize]].forEach((element) => {
+                let display = element[0];
+                let defaultFontSize = element[1];
+                let displayWidth = parseFloat(window.getComputedStyle(displayParent).getPropertyValue('width'));
+                let elementWidth = parseFloat(window.getComputedStyle(display).getPropertyValue('width'));
+                let elementFontSize = parseFloat(window.getComputedStyle(display).getPropertyValue('font-size'));
+                let newFont = (displayWidth / elementWidth) * elementFontSize;
+                if (newFont <= defaultFontSize)
+                    display.style.fontSize = `${newFont}px`;
+                
+            })
+            
+        })
+    })
+}
 
+function resetAll(){
+    currentDisplay.textContent = DEFAULT_CHARACTER;
+    operationDisplay.textContent = '';
+    operationCompleted = false;
+    resetCurrentDisplay = false;
+    doingOperation = false;
 }
 
 function findOperationIndex(){
-    for(var i = 0; i < operationDisplay.textContent.length; i++){
-        if(isNaN(operationDisplay.textContent.charAt(i)))
-            return i;
+    operationsIndexes = []
+    for(let i = 0; i < operationDisplay.textContent.length; i++){
+        if(operations.indexOf(operationDisplay.textContent.charAt(i)) != -1)
+            operationsIndexes.push(i);
     }
-    return -1;
+    if(operationsIndexes.length > 1)
+        return operationsIndexes[1];
+    else 
+        return operationsIndexes[0];
 }
 
 function replaceFirstOperationNumber(numberToReplaceWith){
@@ -72,12 +134,11 @@ function replaceFirstOperationNumber(numberToReplaceWith){
 }
 
 function computeResult(){
-    if(operationDisplay.textContent.slice(-1) == EQUALS_CHARACTER)
+    if(operationCompleted){
+        replaceFirstOperationNumber(currentDisplay.textContent);
         return eval(operationDisplay.textContent.slice(0,-1));
-    number1 = operationDisplay.textContent.slice(0,operationDisplay.textContent.length-1);
-    number2 = currentDisplay.textContent;
-    operation = operationDisplay.textContent.charAt(operationDisplay.textContent.length-1);
-    return eval(`${number1}${operation}${number2}`);
+    }
+    return eval(operationDisplay.textContent+currentDisplay.textContent);
 }
 
 function writeNumberToDisplay(numberToAppend){
